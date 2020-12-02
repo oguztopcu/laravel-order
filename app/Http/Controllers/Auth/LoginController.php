01a17f35dd7025acfcff56c6c7267b4c1aced701
Oguz <oguztopcu7@gmail.com>
2020-12-03 00:12:50 +0300

    login method

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends Controller
 {
-    //
+    /**
+     * Login Controller Class
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('login');
+    }
+
+    /**
+     * Kullanıcı sisteme giriş yapar.
+     *
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->only(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json([
+                'message' => 'E-posta adresiniz veya şifreniz yanlış, lütfen tekrar deneyin'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
 }
