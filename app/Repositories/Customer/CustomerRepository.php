5175b7840b22eac9ba04cdb0f381d414600dc1fd
Oguz <oguztopcu7@gmail.com>
2020-12-03 02:38:09 +0300

    phpDoc added

--- app/Repositories/Customer/CustomerRepository.php
+++ app/Repositories/Customer/CustomerRepository.php
@@ -10,11 +10,24 @@ class CustomerRepository extends Repository
 {
     protected $model = Customer::class;
 
+    /**
+     * Şirkete ait tüm müşterileri listeler
+     *
+     * @param int $companyId
+     * @param int $perPage
+     * @return mixed
+     */
     public function paginate($companyId, $perPage = 10)
     {
         return $this->model()->where('company_id', '=', $companyId)->paginate($perPage);
     }
 
+    /**
+     * Yeni müşteri ekler
+     *
+     * @param Request $request
+     * @return mixed
+     */
     public function create(Request $request)
     {
         $customer = $this->model()->create([
@@ -25,6 +38,13 @@ class CustomerRepository extends Repository
         return $customer;
     }
 
+    /**
+     * Varolan müşteriyi günceller
+     *
+     * @param Customer $customer
+     * @param Request $request
+     * @return Customer
+     */
     public function update(Customer $customer, Request $request)
     {
         $customer->update([
@@ -34,6 +54,12 @@ class CustomerRepository extends Repository
         return $customer;
     }
 
+    /**
+     * Müşteriyi siler
+     *
+     * @param Customer $customer
+     * @throws \Exception
+     */
     public function delete(Customer $customer)
     {
         $customer->delete();
