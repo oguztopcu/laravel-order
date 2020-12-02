287d5eeae72a4b2b910bc8c354665003628b3c37
Oguz <oguztopcu7@gmail.com>
2020-12-03 01:26:53 +0300

    Customer Repository

---
+++ app/Repositories/Customer/CustomerRepository.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Repositories\Customer;
+
+use App\Models\Customer;
+use App\Repositories\Repository;
+use Illuminate\Http\Request;
+
+class CustomerRepository extends Repository
+{
+    protected $model = Customer::class;
+
+    public function paginate($perPage = 10)
+    {
+        return $this->model()->paginate($perPage);
+    }
+
+    public function create(Request $request)
+    {
+        $customer = $this->model()->create([
+            'company_id' => $request->input('company_id'),
+            'name' => $request->input('name')
+        ]);
+
+        return $customer;
+    }
+
+    public function update(Customer $customer, Request $request)
+    {
+        $customer->update([
+            'name' => $request->input('name')
+        ]);
+
+        return $customer;
+    }
+
+    public function delete(Customer $customer)
+    {
+        $customer->delete();
+    }
+}
